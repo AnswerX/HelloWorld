d492bcd4cc9f0cbbb72384a78da080e767e0e323
AnswerX <xurui2011moon@163.com>
2016-03-19 11:06:32 +0800

    test-version 1.0

---
+++ HelloWorld/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HelloWorld
+//
+//  Created by xurui on 16/3/19.
+//  Copyright © 2016年 Answerx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ HelloWorld/ViewController.m
@@ -0,0 +1,33 @@
+//
+//  ViewController.m
+//  HelloWorld
+//
+//  Created by xurui on 16/3/19.
+//  Copyright © 2016年 Answerx. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    
+    NSLog(@"test-version 1.0");
+    
+
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
